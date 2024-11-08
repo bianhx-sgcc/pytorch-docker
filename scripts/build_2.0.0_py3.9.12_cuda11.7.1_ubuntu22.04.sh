5157646c13df0a1189aaecd98791f6fd19ad7629
bianhx-sgcc <63846084+bianhx-sgcc@users.noreply.github.com>
2024-11-08 12:47:00 +0800

    Update build_2.0.0_py3.9.12_cuda11.7.1_ubuntu22.04.sh

--- scripts/build_2.0.0_py3.9.12_cuda11.7.1_ubuntu22.04.sh
+++ scripts/build_2.0.0_py3.9.12_cuda11.7.1_ubuntu22.04.sh
@@ -2,7 +2,7 @@
 
 export BASE_IMAGE=ubuntu:22.04
 
-export PYTHON_VERSION=3.9.12
+export PYTHON_VERSION=3.10.15
 
 export PYTORCH_VERSION=2.0.0
 export PYTORCH_VERSION_SUFFIX=+cu117
@@ -12,6 +12,6 @@ export TORCHAUDIO_VERSION=2.0.0
 export TORCHAUDIO_VERSION_SUFFIX=+cu117
 export PYTORCH_DOWNLOAD_URL=https://download.pytorch.org/whl/cu117/torch_stable.html
 
-export IMAGE_TAG=2.0.0-py3.9.12-cuda11.7.1-ubuntu22.04
+export IMAGE_TAG=2.0.0-py3.10.15-cuda11.7.1-ubuntu22.04
 
 ./docker/ubuntu/build.sh
